415b8f0613621e00029b1d2d35e7c0d62fa6087d
Manuel de Villena Millan Miguel <mvillena@uliege.be>
2021-12-08 17:14:28 +0000

    minor refactor

--- forecast/models/models_sklearn/sklearn_random_forests.py
+++ forecast/models/models_sklearn/sklearn_random_forests.py
@@ -16,9 +16,10 @@ class SKLRandomForest(BaseModelSKLearn):
         super().__init__(features)
         self.model = RandomForestRegressor(
             n_estimators=self.estimators, criterion=self.criterion_trees, max_depth=None, min_samples_split=2,
-                       min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_features='auto', max_leaf_nodes=None,
-                       min_impurity_decrease=0.0, bootstrap=True, oob_score=False, n_jobs=self.jobs, random_state=None,
-                       verbose=0, warm_start=False, ccp_alpha=0.0, max_samples=None)
+            min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_features='auto', max_leaf_nodes=None,
+            min_impurity_decrease=0.0, bootstrap=True, oob_score=False, n_jobs=self.jobs, random_state=None,
+            verbose=0, warm_start=False, ccp_alpha=0.0, max_samples=None
+        )
 
     def train(self):
         """
